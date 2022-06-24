58bb72d14999a8919f66737df1a8097a48ae91c6
Haotian Ma <hm443@cornell.edu>
2022-06-24 19:36:52 -0400

    Update

--- static/components.js
+++ static/components.js
@@ -1,13 +1,13 @@
 
-class Node{
-	constructor(name_, architect_, year_, location_, continent_,typeStr_ ,aspectsStr_, imgUrl_, sourceUrl_, descript_, typeIcon_){
+class Node {
+	constructor(name_, architect_, year_, location_, continent_, typeStr_, aspectsStr_, imgUrl_, sourceUrl_, descript_, typeIcon_) {
 		this.name = name_;
 		this.year = year_;
 		this.location = location_;
 		this.continent = continent_;
 		this.typeStr = typeStr_;
 		this.architect = architect_;
-		this.aspectsStr=aspectsStr_;
+		this.aspectsStr = aspectsStr_;
 		this.imgUrl = imgUrl_;
 		this.sourceUrl = sourceUrl_;
 		this.descript = descript_;
@@ -18,13 +18,13 @@ class Node{
 		this.iconSize = 20;
 
 		this.edgeCluster = [];
-		
+
 		//Moving settings
-		this.p = new p5.Vector(random(windowWidth/2 - windowWidth/5, windowWidth/2 + windowWidth/5),random(windowHeight/2 - windowHeight/5,windowHeight/2 + windowHeight/5));
-		this.u = new p5.Vector(0,0);
-		this.f = new p5.Vector(0,0);
-		this.dt = min(0.5, windowWidth/1920);
-		this.damping = min(0.3, windowWidth/1920);
+		this.p = new p5.Vector(random(windowWidth / 2 - windowWidth / 20 -200, windowWidth / 2 + windowWidth / 20 -200), random(windowHeight / 2 - windowHeight / 20, windowHeight / 2 + windowHeight / 20));
+		this.u = new p5.Vector(0, 0);
+		this.f = new p5.Vector(0, 0);
+		this.dt = min(0.5, windowWidth / 1920);
+		this.damping = min(0.3, windowWidth / 1920);
 		this.isFixed = false;
 
 		//Display settings
@@ -35,15 +35,15 @@ class Node{
 
 		this.isHover = false;
 		this.d = 20;
-		this.color = random(50,255);
-		this.strokeWidth = 0.5;	
-     	this.strokeColor = (200,200,200);
+		this.color = random(50, 255);
+		this.strokeWidth = 0.5;
+		this.strokeColor = (200, 200, 200);
 
 		this.isExpand = false;
 		this.popUp = 0;
 	}
 
-	AddForceTo(sourceNode_, sourceStrength_){
+	AddForceTo(sourceNode_, sourceStrength_) {
 		let sourceNode = sourceNode_;
 		let sourceStrength = sourceStrength_;
 
@@ -56,58 +56,58 @@ class Node{
 		this.f = this.f.add(dv.div(dist).mult(sourceStrength));
 	}
 
-	Move(){
+	Move() {
 		// check fixed
-	    if (this.isFixed) return;
+		if (this.isFixed) return;
 
-	  	// Movement
-	    this.u = this.u.mult(this.damping);
-	    this.u = this.u.add(this.f.mult(this.dt/1));
-	    this.p = this.p.add(this.u.mult(this.dt));
+		// Movement
+		this.u = this.u.mult(this.damping);
+		this.u = this.u.add(this.f.mult(this.dt / 1));
+		this.p = this.p.add(this.u.mult(this.dt));
 	}
 
-	display(){
+	display() {
 		//expand info box
 
 		// if(this.isHidden){
 		// 	this.isExpand = false;
 		// }
 
-		if(this.isExpand){
-				strokeWeight(5);
-				noFill();
-				stroke(225);
-				ellipse(this.p.x, this.p.y, 20);
-				push();
-				noStroke();
-				fill(255,255,255);
-				textSize(12);
-				let textW =textWidth(this.name);
-				text(this.name, this.p.x -textW/2, this.p.y -20);
-		}			
-		
+		if (this.isExpand) {
+			strokeWeight(5);
+			noFill();
+			stroke(225);
+			ellipse(this.p.x, this.p.y, 20);
+			push();
+			noStroke();
+			fill(255, 255, 255);
+			textSize(12);
+			let textW = textWidth(this.name);
+			text(this.name, this.p.x - textW / 2, this.p.y - 20);
+		}
+
 		push();
 
-		if(!this.isHidden){
+		if (!this.isHidden) {
 
-			if(this.isHover){
+			if (this.isHover) {
 				fill(255);
 				strokeWeight(5);
-				stroke(220,220,220);
+				stroke(220, 220, 220);
 				ellipse(this.p.x, this.p.y, 20);
 				strokeWeight(3);
-				stroke(220,220,220);
+				stroke(220, 220, 220);
 				push();
 				noStroke();
-				fill(255,255,255);
+				fill(255, 255, 255);
 				textSize(12);
-				let textW =textWidth(this.name);
-				text(this.name, this.p.x -textW/2, this.p.y -20);
+				let textW = textWidth(this.name);
+				text(this.name, this.p.x - textW / 2, this.p.y - 20);
 				pop();
 			}
-			else{
+			else {
 				strokeWeight(1);
-				stroke(50,200,200);
+				stroke(50, 200, 200);
 			}
 
 			//sort Year test
@@ -115,28 +115,28 @@ class Node{
 
 			//Icon Display
 			fill(255);
-			image(this.typeIcon,this.p.x-(this.iconSize/2),this.p.y-(this.iconSize/2),this.iconSize,this.iconSize);
+			image(this.typeIcon, this.p.x - (this.iconSize / 2), this.p.y - (this.iconSize / 2), this.iconSize, this.iconSize);
 			noStroke();
 
 			fill(255);
 			noStroke();
- 			pop();
-			
+			pop();
+
 			//Circle display
 			// circle(this.p.x,this.p.y,this.d);
 
 		}
 		pop();
 
-		
+
 	}
 
 
-	toggleExpand(){
-		if(!this.isHidden && this.isExpand==false){
+	toggleExpand() {
+		if (!this.isHidden && this.isExpand == false) {
 			this.isExpand = true;
 		}
-		else{
+		else {
 			this.isExpand = false;
 		}
 	}
@@ -145,63 +145,63 @@ class Node{
 	checkEdges(xW, yH, width, height) {
 		if (this.p.x > width) {
 			this.p.x = width;
-		  	this.u.x *= -1;
+			this.u.x *= -1;
 		} else if (this.p.x < xW) {
 			this.p.x = xW;
 			this.u.x *= -1;
 		}
-	 
+
 		if (this.p.y > height) {
 			this.p.y = height;
 			this.u.y *= -1;
-		}else if (this.p.y < yH){
+		} else if (this.p.y < yH) {
 			this.p.y = yH;
 			this.u.y *= -1;
 		}
-	  }
+	}
 
-	description(){
+	description() {
 		textSize(12);
-        text('selected: ' + this.name,(this.p.x+this.d/2)+5, this.p.y);
-        text('by: ' + this.architect,(this.p.x+this.d/2)+5, this.p.y+15);
-        noStroke();
-        image(img, 650, 50, 100);
-        text(this.name, 650, 150);
-        text("architect: " + this.architect, 650,165);
-        textSize(10);
-        for(let i=0;i<this.aspectsStr.length;i++){
-            text(this.aspectsStr[i], 650, 190+(i*15)); 
-        }
-        textSize(8);
-        text(this.descript, 620, 195+(15*this.aspectsStr.length), 160, 500);
+		text('selected: ' + this.name, (this.p.x + this.d / 2) + 5, this.p.y);
+		text('by: ' + this.architect, (this.p.x + this.d / 2) + 5, this.p.y + 15);
+		noStroke();
+		image(img, 650, 50, 100);
+		text(this.name, 650, 150);
+		text("architect: " + this.architect, 650, 165);
+		textSize(10);
+		for (let i = 0; i < this.aspectsStr.length; i++) {
+			text(this.aspectsStr[i], 650, 190 + (i * 15));
+		}
+		textSize(8);
+		text(this.descript, 620, 195 + (15 * this.aspectsStr.length), 160, 500);
 	}
 }
 
-class Edge{
-	constructor(node_, aspect_,){
+class Edge {
+	constructor(node_, aspect_,) {
 		this.node = node_;
 		this.aspect = aspect_;
 	}
-	ApplyForce(){
+	ApplyForce() {
 		let dv = p5.Vector.sub(this.aspect.p, this.node.p);
 		let dist = dv.mag();
 		dv.normalize();
-		
+
 		//here control force, speed
-		this.node.f =this.node.f.add(dv.mult(((0.5*dist)/100-0.5)));
+		this.node.f = this.node.f.add(dv.mult(((0.5 * dist) / 100 - 0.5)));
 	}
 
-	display(){
-		if(!this.node.isHover){
+	display() {
+		if (!this.node.isHover) {
 			strokeWeight(this.aspect.strokeWidth);
 		}
 		stroke(this.aspect.edgeColor);
-		line(this.node.p.x,this.node.p.y, this.aspect.p.x,this.aspect.p.y);
+		line(this.node.p.x, this.node.p.y, this.aspect.p.x, this.aspect.p.y);
 	}
 }
 
-class EdgeCluster{
-	constructor(node_, aspects_,){
+class EdgeCluster {
+	constructor(node_, aspects_,) {
 		this.node = node_;
 		this.aspects = aspects_;
 		this.edges = [];
@@ -210,103 +210,108 @@ class EdgeCluster{
 		this.isHover = false;
 	}
 
-	display(){
-		if(!this.node.isHidden){
-		push();
-		if(this.isHover){
-			strokeWeight(3);			
-			stroke(255,255,255);
+	display() {
+		if (!this.node.isHidden) {
+			push();
+			if (this.isHover) {
+				strokeWeight(3);
+				stroke(255, 255, 255);
 
-		}else{
-			strokeWeight(0.5);
+			} else {
+				strokeWeight(0.5);
+			}
+			this.edges.forEach(edge => edge.display());
+			pop();
+		} else {
+			strokeWeight(0.1);
+
 		}
-		this.edges.forEach(edge => edge.display());
-		pop();
-	}else {
-		strokeWeight(0.1);
-		
-	}
 
-}
-	ApplyForce(){
+	}
+	ApplyForce() {
 		this.edges.forEach(edge => edge.ApplyForce());
 	}
 }
 
-class Aspect{
-	constructor(name_,colorIdle_){
+class Aspect {
+	constructor(name_, colorIdle_) {
 		this.name = name_;
 		this.isDrag = false;
 		this.isHover = false;
 
 		//Location
-		this.p = new p5.Vector(random(100,windowWidth-300),random(100,windowHeight-100));
+		this.p = new p5.Vector(random(100, windowWidth - 300), random(100, windowHeight - 100));
 
 		//Display settings
 		this.d = 40;
 		this.colorIdle = colorIdle_;
-		this.colorDrag = [255,255,50];	
-		this.strokeWidth = 0.5;	
-     	this.strokeColor = (200,200,200);
-		
+		this.colorDrag = [255, 255, 50];
+		this.strokeWidth = 0.5;
+		this.strokeColor = (200, 200, 200);
+
 		this.edgeColor = this.colorIdle;
 		this.sine = 0;
 	}
 
-	display(){
+	display() {
 
-		if (this.p.x > canvasXW-50) {
-			this.p.x = canvasXW-50;
+		if (this.p.x > canvasXW - 50) {
+			this.p.x = canvasXW - 50;
 		} else if (this.p.x < 50) {
 			this.p.x = 50;
 		}
-	 
+
 		if (this.p.y > canvasYH - 50) {
 			this.p.y = canvasYH - 50;
-		}else if (this.p.y < 50){
+		} else if (this.p.y < 50) {
 			this.p.y = 50;
 		}
-	  
+
 		push();
 
 		fill(255);
 		noStroke();
 
 
-    
-		if(this.isDrag){
+
+		if (this.isDrag) {
 			this.p.x = mouseX;
 			this.p.y = mouseY;
 			// fill(this.colorDrag[0],this.colorDrag[1],this.colorDrag[2]);
-
-
 			stroke(this.colorIdle);
 			strokeWeight(0.5);
-			fill(0,0,0,0);
-			circle(this.p.x, this.p.y, this.d+this.sine);
+			fill(0, 0, 0, 0);
+			circle(this.p.x, this.p.y, this.d + this.sine);
 			this.sine++;
 			fill(this.colorIdle);
-
-		}else{
+		} else {
 			// fill(this.colorIdle[0],this.colorIdle[1],this.colorIdle[2]);
 			fill(0);
-			
 		}
-
 		push();
-		if(this.isHover){
-			this.strokeWidth= 2;
+		if (this.isHover) {
+			this.strokeWidth = 2;
 			strokeWeight(0.5);
 			stroke(this.colorIdle);
-			fill(0,0,0,0);
-			circle(this.p.x, this.p.y, this.d+this.sine);
+			fill(0, 0, 0, 0);
+			circle(this.p.x, this.p.y, this.d + this.sine);
+			if (this.sine > 200) {
+				this.sine = 0
+			}
 			this.sine++;
-		}
-
-		else{
+		} else {
 			this.edgeColor = this.colorIdle;
-			this.strokeWidth= 0.5;
-			this.sine=0;
+			this.strokeWidth = 0.5;
+			if (this.sine != 0 && this.sine < 500) {
+				strokeWeight(0.5);
+				stroke(this.colorIdle);
+				fill(0, 0, 0, 0);
+				circle(this.p.x, this.p.y, this.d + this.sine);
+				this.sine++;
+			} else if (this.sine >= 500) {
+				this.sine = 0;
+			}
+			// this.sine = 0;
 		}
 		pop();
 
@@ -314,13 +319,13 @@ class Aspect{
 		stroke(255);
 		fill(this.colorIdle);
 		strokeWeight(2);
-		circle(this.p.x,this.p.y, this.d/1.5);
-		
+		circle(this.p.x, this.p.y, this.d / 1.5);
+
 		push();
-		if(this.isHover){
+		if (this.isHover) {
 			noStroke();
 			fill(0)
-			circle(this.p.x, this.p.y, this.d/2);
+			circle(this.p.x, this.p.y, this.d / 2);
 		}
 		pop();
 
@@ -328,14 +333,14 @@ class Aspect{
 		let textW = textWidth(this.name);
 		stroke(0);
 		textSize(16);
-		text(this.name, this.p.x - (textW/2 -2), this.p.y - 25);
+		text(this.name, this.p.x - (textW / 2 - 2), this.p.y - 25);
 		pop();
 
-		}
 	}
+}
 
-class Popup_v2{
-	constructor(id_, node_){
+class Popup_v2 {
+	constructor(id_, node_) {
 		this.id = id_;
 		this.node = node_;
 
@@ -349,24 +354,24 @@ class Popup_v2{
 		this.div.id(this.id);
 		this.div.parent(this.container);
 		this.div.class('popup');
-	
+
 		//popup fill content
-		this.pfill= "<span id='close' onclick='this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode); return false;'>x</span><h1>"+ this.node.name + "</h1><h3>" + this.node.architect + "</h3><h3>" + this.node.location + ", " + this.node.year + "</h3><img src='" + this.node.imgUrl + "'/><p>" + this.node.descript + "</p>";
+		this.pfill = "<span id='close' onclick='this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode); return false;'>x</span><h1>" + this.node.name + "</h1><h3>" + this.node.architect + "</h3><h3>" + this.node.location + ", " + this.node.year + "</h3><img src='" + this.node.imgUrl + "'/><p>" + this.node.descript + "</p>";
 
 		this.div.html(this.pfill);
 	}
 
-	
 
 
-	updatePopup(){
+
+	updatePopup() {
 		this.container.hide();
-	 }
+	}
 
 }
 
-class Type{
-	constructor(typeStr_, typeIcon_){
+class Type {
+	constructor(typeStr_, typeIcon_) {
 		this.typeStr = typeStr_;
 		this.typeIcon = typeIcon_;
 	}
